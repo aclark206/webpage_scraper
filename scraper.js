663cdc49dc6f77d7bc22a72b0f14d06d1702c64e
aclark206 <starbarrett@gmail.com>
2018-09-19 16:01:09 -0700

    added scraper.js

---
+++ scraper.js
@@ -0,0 +1,17 @@
+/* 
+webpage_scraper 
+
+Combs a webpage for product, price, imageURL, URL, and Time and puts them into a csvfile.  
+
+Accepts the URL of the webpage to scrape.
+
+Saves the CSV file in the ./data sub directory.  If ./data does not already exist, it creates one.
+ 
+The csv file is named with the date stamp of it's creation.  e.g  2016-11-21.csv  IF the file already exists, it will be written over.
+
+Errors will be logged to ./data/scraper-error.log
+
+
+*/
+
+
