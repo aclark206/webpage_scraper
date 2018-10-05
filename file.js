ec038597f3c1dfa1f8b4237ae18e3423410a2c4d
aclark206 <starbarrett@gmail.com>
2018-10-05 16:08:42 -0700

    Initial functionality
    scrapes data from a webpage and puts the data into an csv file

---
+++ file.js
@@ -0,0 +1,169 @@
+/*
+*  @file file.js
+*    holds all the file management functions for scraper.js
+
+*  @function checkDir 
+*  @function createFile
+*  @function getTodaysDate
+*  @function logError
+* 
+*/
+
+/*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
+*			Dependencies
+*/
+fs = require('fs');
+
+
+
+/*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
+*			Functions
+*/
+
+
+/* - - - 
+* 
+* @function checkDir
+*    Check to see if the directory already exists.  If it doesn't, it creates it
+*
+* @param {string} path:  path of the directory to be checked
+*/
+// Check to see if the directory already exists.  If it doesn't, it creates it
+var checkDir = function(path) {
+
+	return new Promise (function (resolve, reject) {
+		fs.mkdir(path, function (error) {
+			if (!error) {// directory did not previously exist and was successfully created
+				console.log("path" + path + " created");
+				resolve(true);
+			}
+			else if (error.code == 'EEXIST'){  // directory existed 
+				console.log("path" + path + " already exists");
+				resolve(true);
+			}
+			else {// something went wrong.  Handle error
+				fileManager.getTodaysDate()
+					.then(date => fileManager.logError(date + " -- " + error.message, path));
+				reject(error);
+			}
+		});
+	});
+}
+
+
+
+/* - - - 
+* 
+* @function createFile
+*    creates a file of the name fileName.  Adds the content to the file.  If a file of the same *	name exists, it writes over the file.    
+*
+*  @returns {Promise} Returns a Promise with a boolean true if successful or an Error if not
+*
+* @param {string} path:  path of the directory to be checked
+* @param {string} fileName: name of the file to be created
+* @param {string} content: what is saved into the file
+*/
+
+var createFile = function(path, fileName, content){
+
+	return new Promise (function (resolve, reject) {
+		fs.writeFile(path + '/' + fileName, content, function (error) {
+			if (!error) {// directory did not previously exist and was successfully created
+				console.log(path + '/' + fileName + ' created');
+				resolve(true);
+			}
+			else if (error.code == 'EEXIST'){  // directory existed 
+				console.log(path + '/' + fileName + ' created');
+				resolve(true);
+			}
+			else {// something went wrong.  Handle error
+				//logError(error.message);
+				getTodaysDate()
+					.then(date => fileManager.logError(date + " -- " + error.message, path));
+				reject(error);
+			}
+		});
+	});
+	
+};
+
+
+/*
+*	@function getTodaysDate
+*		today's date formatted day-month-4digityear e.g 1-10-2018
+*
+*  	@returns a Promise with string holding
+*	
+*/
+var getTodaysDate = function () {
+
+	return new Promise (function (resolve, reject) {
+		const today = new Date();
+		let todayFormatted = today.getDate() + "-" 
+							+ (today.getMonth() + 1) + "-"
+							+ today.getFullYear();
+		resolve(todayFormatted);
+	});	
+};
+
+
+/* - - - 
+*
+*	@function logError
+*   	Passed Errors will be logged to ./${path}/scraper-error.log
+*   	if either the data directory or the log are not previously existing, they will be created.
+*		errors ocurring during this function are logged to the console.
+*
+*	@params {string} errorMsg: is a string with the message that will be logged to the error log.
+* 	@params {string} path: the path to the directory holding the error log 
+*	
+*/
+var logError = function (errorMsg, path) {
+	checkDir(path) // make sure data directory exists
+		.then(fs.appendFile(path + '/scraper-error.log', errorMsg, error => {
+			if (error) {
+				console.log('ERROR creating scraper-error.log: ' + error.code);
+				throw err;
+			}
+			}))
+		.catch(error => { throw error; console.log(error.message)});
+}
+
+
+/* - - - 
+* 
+* @function logCSVRecord
+*    writes the passed record to the csv file 
+*
+*  @returns {Promise} Returns a Promise with a boolean true if successful or an Error if not
+*
+* @param {string} record: a csv formated string to be written to the file
+* @param {string} path:  path of the directory to the csv file
+* @param {string} fileName: name of the csv file
+
+*/
+var logCSVRecord = function (record, path, fileName) {
+	return new Promise (function (resolve, reject){
+		getTodaysDate()
+			.then(date => fs.appendFile(path + '/' + date + '.csv', record, error => {
+				if(error) {
+					getTodaysDate()
+					.then(date => fileManager.logError(date + " -- " + error.message, path));
+					reject(error); 
+				}
+				else
+					resolve(true)
+			}));
+	}) // end Promise	
+} // end function
+
+
+/*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
+*			Exports
+*/
+module.exports.checkDir = checkDir;
+module.exports.getTodaysDate = getTodaysDate;
+module.exports.createFile = createFile;
+module.exports.logError = logError;
+module.exports.logCSVRecord = logCSVRecord;
+
--- scraper.js
+++ scraper.js
@@ -1,17 +1,131 @@
 /* 
-webpage_scraper 
-
-Combs a webpage for product, price, imageURL, URL, and Time and puts them into a csvfile.  
-
-Accepts the URL of the webpage to scrape.
-
-Saves the CSV file in the ./data sub directory.  If ./data does not already exist, it creates one.
- 
-The csv file is named with the date stamp of it's creation.  e.g  2016-11-21.csv  IF the file already exists, it will be written over.
-
-Errors will be logged to ./data/scraper-error.log
-
+@file scraper.js
+    * Combs a webpage for product, price, imageURL, URL, and Time and puts them into a csvfile.  
+	
+    *Saves the CSV file in the ./data sub directory.  If ./data does not already exist, it creates one.
+	
+    * The csv file is named with the date stamp of it's creation.  e.g  2016-11-21.csv  IF the file already exists, it will be written over.
+	
+    * Errors will be logged to ./data/scraper-error.log
 
 */
 
+/*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+*			Dependencies
+*/
+const scrapeIt = require("scrape-it");
+const fs = require('fs');
+const fileManager = require('./file.js');
+
+
+
+/*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
+*			Helper Functions
+*/
+
+/* - - - 
+* @function createRecord
+* grabs Title, Price, and imageURl from the passed URL, and logs them in CSV format into a
+* file of today's date in the directory indicated by the global variable, 'homeDirectory'.
+* the record will be formatted:  Title, Price, ImageURL, URL, Time 
+* 
+*    @param {string} pageUrl  the URL of the address to scrape
+*/
+
+var createRecord = function (pageUrl){
+		
+// for each URL call this scrapeIt 
+	scrapeIt(pageUrl, {
+		product: {
+			selector: ".shirt-picture img",
+			attr: "alt"
+		},
+		price: ".price",
+		imageURL: {
+			selector: ".shirt-picture img",
+			attr: "src"
+		}
+	}).then(({ data, response }) => {
+		// change the format of the data from json to csv
+		let recordCSV = formatRecord(data, pageUrl);
+		
+		// log the URL and the product, price, imageURL, and time to the CSV file
+		fileManager.logCSVRecord(recordCSV, homeDirectory, fileManager.getTodaysDate() + '.csv');
+	}).catch(error => { 
+			throw error; 
+			fileManager.getTodaysDate()
+			.then(date => fileManager.logError(date + " -- " + error.message, homeDirectory));
+		});
+}
+
+
+/* - - - 
+* @function formatRecord
+*    turns json object into a string formatted: Title, Price, ImageURL, URL, and Time
+*
+*    @param {object} rawData:  object is a json formated string containg
+*								{product: string,
+*								 price: string,
+*								imageURL: string }
+*
+*    @param {string} url: the parent URL to be logged in the CSV file
+*
+*    @returns {string} csv formatted string containing 	 Title, Price, ImageURL, URL, and Time
+*/
+var formatRecord = function (rawData, url){
+	let record = rawData.product.replace(/,/g ,'-') + ", ";
+	record += rawData.price + ", ";
+	record += rawData.imageURL + ", ";
+	record += url  + ", ";
+	let time = new Date();
+	record += time.toLocaleTimeString();
+	record += "\n";
+	return record;
+}
+
+
+
+/*  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  * 
+*			Main
+*/
+let homeDirectory = "./data";
+let URL = "http://shirts4mike.com";
+let header = "Title, Price, imageURL, URL, Time \n";
+
+
+fileManager.checkDir(homeDirectory) // mk directory
+    .then(fileManager.getTodaysDate) 
+	.then(date => fileManager.createFile(homeDirectory, '/' + date + '.csv', header)) 
+    .catch(error => { 
+		throw error; 
+		fileManager.getTodaysDate()
+		.then(date => fileManager.logError(date + " -- " + error.message, homeDirectory));
+	});
+
+	
+// call the first page and create an array of shirt URLS
+scrapeIt(URL + "/shirts.php", {
+	shirtUrls: {
+	   listItem: ".products li"
+	 , data: {
+		   url: {
+			   selector: "a"
+			 , attr: "href"
+		   } 
+		}	
+	}
+}).then(({ data, response }) => {
+	if (response.statusCode == 200){
+		//iterate over the data
+		for (let i = 0; i < data.shirtUrls.length; i++){
+			createRecord(URL + '/' + data.shirtUrls[i].url);
+		}
+		console.log('All records logged to csv file.');
+	}
+}).catch(error => { 
+		throw error; 
+		fileManager.getTodaysDate()
+		.then(date => fileManager.logError(date + " -- " + error.message, homeDirectory));
+	}); 
+	
 
